fd1923d4d4468d9ebc38d52e1afda1bd8d65c9a2
hedonvictor <hedonbresciani@gmail.com>
2024-08-30 13:57:03 -0300

    adding coments

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,7 @@ const Stack = createNativeStackNavigator();
 function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home"> {/* definição da tela inicial */}
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Barbers" component={BarbersScreen} />
       </Stack.Navigator>
