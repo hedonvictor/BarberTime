34d05bb767fbbb5d44ddc1bf0337f69894fdaf06
hedonvictor <hedonbresciani@gmail.com>
2024-09-02 20:32:47 -0300

    switch custom colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,6 +1,8 @@
 export default {
     customBlack: '#353535',
-    customBlue: '#004E98',
-    customRed: '#AE2012',
-    customWhite: '#EFEFD0'
+    customWhite: '#EFEFD0',
+    customGreenMoss: '#A274e13',
+    customGreen: '#b5e550',
+    customGreen2: '#38761d',
+    customGreen3: '#6aa84f'
 };
